3602f034b14651ad9c710858dbf79f2c3eef4c5e
sky-ADMIN <546485299@qq.com>
2016-12-15 09:03:06 +0800

    暂存

---
+++ lesson5-vue/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by lhy95 on 2016/12/14.
+ */
+
+new Vue({
+    el: '.container',
+    mounted() {
+        console.log(123);
+        this.$http.get('http://115.159.184.76:3001/api/comments').then(function (res) {
+            console.log(res.data);
+        })
+    }
+});
