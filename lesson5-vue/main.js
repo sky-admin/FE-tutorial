8c62d3baa1653bd16e343f177b74e0fa06218f14
sky-ADMIN <546485299@qq.com>
2016-12-15 12:50:58 +0800

    Vue第一步完工

--- lesson5-vue/main.js
+++ lesson5-vue/main.js
@@ -4,10 +4,14 @@
 
 new Vue({
     el: '.container',
+    data: {
+        list: []
+    },
     mounted() {
-        console.log(123);
+        let that = this;
         this.$http.get('http://115.159.184.76:3001/api/comments').then(function (res) {
             console.log(res.data);
+            that.list = res.data;
         })
     }
 });
